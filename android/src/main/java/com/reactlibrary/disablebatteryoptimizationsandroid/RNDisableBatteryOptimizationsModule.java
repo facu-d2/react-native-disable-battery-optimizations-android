a36da7022003065eb550c42813c915a4ec23b42e
Kévin JEAN <rasen414@gmail.com>
2024-11-05 21:57:53 +0100

    callback to know if the user accept or deny permission

--- android/src/main/java/com/reactlibrary/disablebatteryoptimizationsandroid/RNDisableBatteryOptimizationsModule.java
+++ android/src/main/java/com/reactlibrary/disablebatteryoptimizationsandroid/RNDisableBatteryOptimizationsModule.java
@@ -1,10 +1,12 @@
 package com.reactlibrary.disablebatteryoptimizationsandroid;
 
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.provider.Settings;
 import android.os.PowerManager;
@@ -12,27 +14,31 @@ import android.net.Uri;
 import android.os.Build;
 
 
-public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaModule {
+public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
   private final ReactApplicationContext reactContext;
+    private static final int BATTERY_OPTIMIZATION_REQUEST_CODE = 1234;
+  private Promise batteryPromise;
 
-  public RNDisableBatteryOptimizationsModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
+    public RNDisableBatteryOptimizationsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        reactContext.addActivityEventListener(this); // Ajout de l'écouteur d'événements
+    }
   @ReactMethod
-  public void openBatteryModal() {
-	  if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-		    String packageName = reactContext.getPackageName();
-			Intent intent = new Intent();
-			intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-			intent.setData(Uri.parse("package:" + packageName));
-			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			reactContext.startActivity(intent);
-    
-	  }
-
+  public void openBatteryModal(Promise promise) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+          String packageName = reactContext.getPackageName();
+          Intent intent = new Intent();
+          intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+          intent.setData(Uri.parse("package:" + packageName));
+          intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          this.batteryPromise = promise;
+  
+          reactContext.startActivityForResult(intent, BATTERY_OPTIMIZATION_REQUEST_CODE, null);
+      } else {
+          promise.resolve(true);
+      }
   }
 
   @ReactMethod
@@ -42,13 +48,26 @@ public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaMod
         PowerManager pm = (PowerManager) reactContext.getSystemService(reactContext.POWER_SERVICE);
         if (!pm.isIgnoringBatteryOptimizations(packageName)) {
           promise.resolve(true);
-          return ;
+          return;
         }
     }
     promise.resolve(false);
   }
 
-
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+      if (requestCode == BATTERY_OPTIMIZATION_REQUEST_CODE) {
+          if (batteryPromise != null) {
+              // Vérifiez le résultat
+              if (resultCode == Activity.RESULT_OK) {
+                  batteryPromise.resolve(true);
+              } else {
+                  batteryPromise.resolve(false);
+              }
+              batteryPromise = null; // Réinitialisez la promesse
+          }
+      }
+  }
 
 
   @ReactMethod
